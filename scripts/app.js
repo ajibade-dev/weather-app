df2fb8bab9fc56e1fb4d4dfae50d9ca18e2fd86e
unknown <ajibadeifedolapo17@gmail.com>
2022-09-30 07:57:47 +0100

    Fourth Project

---
+++ scripts/app.js
@@ -0,0 +1,83 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const timeNow = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+
+const updateUI = (data) => {
+
+   // const cityWay = data.cityWay;
+    //const weather = data.weather;
+
+    //destructuring
+
+    const {cityWay, weather} = data;
+
+    //update details template
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityWay.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+// update the night/day and icons image
+
+const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+icon.setAttribute('src', iconSrc);
+
+
+let timeSrc = null;
+
+if(weather.IsDayTime){
+    timeSrc = 'img/day.svg';
+}else {
+    timeSrc = 'img/night.svg';
+}
+
+timeNow.setAttribute('src', timeSrc);
+
+
+   //remove the d-none class if present
+   
+  if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none'); 
+  }
+}
+
+
+
+
+
+
+const updateCity = async (city) => {
+
+    const cityWay = await getCity(city);
+    const weather = await getWeather(cityWay.Key); 
+
+    return{
+     cityWay,
+     weather
+    };
+
+};
+
+  cityForm.addEventListener('submit', e => {
+//prevent default reload
+e.preventDefault();
+
+//get city value
+const city = cityForm.city.value.trim();
+
+cityForm.reset();
+
+//update the ui with new city
+updateCity(city)
+.then(data => updateUI(data))
+.catch(error => console.log(error));
+  })
---
+++ scripts/forecast.js
@@ -0,0 +1,30 @@
+const key = '0VGRAL38y8iTAkHyjEU40GSBEINOT9Xk';
+
+//get weather info
+const getWeather = async (id) => {
+
+const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const query = `${id}?apikey=${key}`;
+
+const response = await fetch(base + query);
+const data = await response.json();
+
+return data[0];
+
+}
+
+//get city info
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+   const query = `?apikey=${key}&q=${city}`;
+
+   const response = await fetch(base + query);
+   const data = await response.json();
+
+    return data[0];
+
+};
+
+
+
